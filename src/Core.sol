a04b6c739879edc4b7aa59c556c879d313d904f1
clemlak <clementlakhal@gmail.com>
2024-01-10 11:39:46 +0400

    feat: add spawn function

--- src/Core.sol
+++ src/Core.sol
@@ -3,7 +3,36 @@ pragma solidity ^0.8.13;
 
 import "solmate/tokens/ERC1155.sol";
 
-contract Core is ERC1155 {
+contract ICore {
+    // Errors
+
+    error AlreadySpawned();
+
+    // Structs
+
+    struct Entity {
+        uint256 x;
+        uint256 y;
+        bool canMove;
+        bool canAttack;
+    }
+}
+
+contract Core is ICore, ERC1155 {
+    mapping(address player => bool) public hasSpawned;
+    mapping(uint256 id => Entity) public entities;
+
+    uint256 entitiesCount;
+
+    function spawn(uint256 x, uint256 y) external {
+        if (hasSpawned[msg.sender]) revert AlreadySpawned();
+        hasSpawned[msg.sender] = true;
+        uint256 entityId = entitiesCount;
+        _mint(msg.sender, entityId, 1, "");
+        entities[entityId] = Entity(x, y, false, false);
+        entitiesCount++;
+    }
+
     function uri(uint256) public pure override returns (string memory) {
         return "";
     }
